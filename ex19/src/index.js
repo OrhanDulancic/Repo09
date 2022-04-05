2de42db03e5ecf93cd7f53fbbe228e43b56cf07f
OrhanDulancic <orhan.dulancic@outlook.com>
2022-04-06 01:46:10 +0200

    Do while loops

---
+++ ex19/src/index.js
@@ -0,0 +1,14 @@
+function myDoWhile() {
+  var myNumbers = "";
+  var i = 0;
+  do {
+    myNumbers += i + ",";
+    i++;
+  } while (i < 9);
+
+  i === 9 ? (myNumbers += i) : (myNumbers += i + ",");
+  i++;
+  return myNumbers;
+}
+
+console.log(myDoWhile());
