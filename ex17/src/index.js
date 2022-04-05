833ca434dbbe4f268c3821f91ea2a786783a14a5
OrhanDulancic <orhan.dulancic@outlook.com>
2022-04-06 01:45:25 +0200

    while loops

---
+++ ex17/src/index.js
@@ -0,0 +1,11 @@
+function inverseWhile () {
+    var fiveNumbers = ""
+    var i = 5;
+    while (i >= 0) {
+        (i === 0) ? fiveNumbers+=i : fiveNumbers +=i + ",";
+        i--;
+        
+    } return fiveNumbers;
+}
+
+console.log(inverseWhile());
